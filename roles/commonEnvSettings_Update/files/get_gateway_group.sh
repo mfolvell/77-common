de51afcff34da916362cd1f656732307838e91e7
Morten Folvell <morten.folvell@sparebank1.no>
2020-05-05 13:30:17 +0200

    slettet unødvendige script.

--- roles/commonEnvSettings_Update/files/get_gateway_group.sh
+++
@@ -1,3 +0,0 @@
-pathTopolopy=$2/groups
-GROUPNO=$(jq --arg inputParam "$1" --slurp '.[].groups[] | select(.name==$inputParam) | .id' $pathTopolopy/topology.json)
-echo "${GROUPNO//'"'}"
--- roles/commonEnvSettings_Update/files/get_gateway_instance.sh
+++
@@ -1,4 +0,0 @@
-pathTopolopy=$1/groups
-INSTANCE=$(jq --arg hostnameShort "$2" --slurp '.[].groups[].services[] | select (.id | contains("instance")) | select(.name==$hostnameShort) |.id' $pathTopolopy/topology.json)
-
-echo "${INSTANCE//'"'}"
