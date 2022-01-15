2ed04316d50b33038b8fca113c8187dee475211e
Morten Folvell <morten.folvell@sparebank1.no>
2022-01-15 12:54:25 +0100

    First alfa version

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
-#﻿#  Fellesbibliotek for Axway relatert ansible
+# Common library
 
-## Bakgrunn og innhold
-Det som ligger her er roller som brukes flere steder.   De jobbene som skal bruke disse rollene må clone ned innholdet i dette repo lokalt. Det gjøres av sh script som kjøre sekve jobben.   Dette gjøre med følgende kode:
-```
-rm -rf  ansible-common
-mkdir  ansible-common
-git clone ssh://git@stash.intern.sparebank1.no:22/axway/ansible-common.git ./ansible-common
-```
-Til slutt i jobben som kjører ansible må Fellesbibliotek slettes.  Det gjøres slik
-```
-# # Fjerner lokal copy
-rm -rf  ./localGitFolder ./ansible-common
-```
-
-### inventories
-Det ligger ikke informasjon om hvilke server som rollene i dette repo skal kjøres på. De ligger i hoved jobben som "kaller" på rollene i fellesbibliotek
-
-## Innhold
-### Roles
-Alle rollene har navn som skal være selvforklarende.   Det er ikke nødvendig med mere info.  
+Documentation under development
--- release.sh
+++ release.sh
@@ -3,11 +3,11 @@ set -e
 set -u
 git remote update
 if git diff-index --quiet HEAD --; then
-   echo "Det er INGEN lokale endringer.  release kan fortsette"
+   echo "There is no local change.  release can continue"
 else
-   echo "ADVARSEL! Din branch haer lokale endringer som IKKE er commitet."
-   echo "Du bør vurdere git pull/push før kjøring av relase"
-   read -p "Forsette [Yn]? " -n 1 -r
+   echo "WARNING! Your branch has diverged from origin."
+   echo "You may consider running git pull/push before releasing"
+   read -p "Continue anyway [Yn]? " -n 1 -r
    echo
    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
      echo "aborted"
@@ -18,13 +18,13 @@ fi
 ver=$( xmllint --xpath "/*[name()='project']/*[name()='version']/text()" pom.xml )
 version=${ver//-SNAPSHOT/}
 
-echo "Lage release for version: "$version
-read -p "Fortsette [Yn]? " -n 1 -r
+echo "Creating release for version: "$version
+read -p "Continue [Yn]? " -n 1 -r
 
 git tag -a "v$version" -m "Releasing version $version"
 
 branch_name="release/v$version"
-echo "Ny release branch" "$branch_name"
+echo "Ny branch branch_name" "$branch_name"
 git branch "release/v$version"
 git push -u origin "$branch_name"
 git co master
